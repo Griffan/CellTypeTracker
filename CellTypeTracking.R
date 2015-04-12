b666d6965c52d72f58daac69000917255257245b
griffan.zhang <zhangfanqw@gmail.com>
2015-04-12 02:31:00 -0400

    add function to generate allele combination

--- CellTypeTracking.R
+++ CellTypeTracking.R
@@ -1,10 +1,12 @@
 setwd("E:/Dropbox/workingspace/CellTypeTracker")
 #assume input file are pileup files
 
-#function that generate all the permutation of genotype of given ploidy
+#function that generate all the unique combination of alleles(genotype) of given ploidy
 generate.genotype.permutation=function(ploidy)
 {
-  alelles=c('A','C','G','T')
+  #alelles=c('A','C','G','T')
+  return(unique(t(combn(c(rep('A',ploidy),rep('C',ploidy),rep('G',ploidy),rep('T',ploidy)),ploidy))))
+  
 }
 
 site.likelihood=function(seq,qual,ploidy)
