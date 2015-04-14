79d2bfe37764994e4b8950fd1b954a819ae54cda
griffan <zhangfanqw@gmail.com>
2015-04-14 14:12:17 -0400

    finished likelihood calculation

--- CellTypeTracking.R
+++ CellTypeTracking.R
@@ -27,23 +27,25 @@ genotype.prior=function(genotype)
 #conditional prob
 conditional.prob=function(base,genotype,error.status)
 {
+#   base="T"
+#   genotype=c("A","A","C","G")
+#   error.status=1
   numA=sum(countCharOccurrences("A",genotype))
   numC=sum(countCharOccurrences("C",genotype))
   numG=sum(countCharOccurrences("G",genotype))
   numT=sum(countCharOccurrences("T",genotype))
   num=c(numA,numC,numG,numT)
-  names(num)=c("A","C","T","G")
+  names(num)=c("A","C","G","T")
   total=length(genotype)
+  k=sum(c(1,1,1,1)[c(numA>0,numC>0,numG>0,numT>0)])# how many kinds of alleles
   if(error.status==1)
   {
-    if(num[base]!=0)
-    {
-      
-    }
+      return(numA/total*1/3+numC/total*1/3+numG/total*1/3+numT/total*1/3-num[base]/total*1/3)
   }
-  else
+  else# no error event
   {
       
+    return(num[base]/total)
   }
 }
 #function that generate all the unique combination of alleles(genotype) of given ploidy
@@ -54,15 +56,16 @@ generate.genotype.permutation=function(ploidy)
   
 }
 
-#multiply all the observed reads for a single site
-single.observation.likelihood(base,quality,alpha,geno1,geno2)
+#marginal likelihood over all error status
+single.observation.likelihood=function(base,quality,alpha,geno1,geno2)
 {
+  quality=strtoi(charToRaw(quality),16L)
   #if error happened
-  likelihood=10^(-quality/10)*((1-alpha)*conditional.prob(base,geno1,1)+alpha*conditional.prob(base,geno2,1))
+  likelihood=10^(0-quality/10)*((1-alpha)*conditional.prob(base,geno1,1)+alpha*conditional.prob(base,geno2,1))
     
   #if error not happened
-  likelihood=likelihood+(1-10^(-quality/10))*((1-alpha)*conditional.prob(base,geno1,0)+alpha*conditional.prob(base,geno2,0))
-  return(likelihood)
+  likelihood=likelihood+(1-10^(0-quality/10))*((1-alpha)*conditional.prob(base,geno1,0)+alpha*conditional.prob(base,geno2,0))
+  return(log(likelihood))
 }
 
 #calculate genotype likelihood for single site, ploidy1 for celltype of interest, ploidy2 for the rest smoothies
@@ -81,17 +84,31 @@ site.likelihood=function(seq,qual,ploidy1,ploidy2,alpha)
       {
         loglik=loglik+single.observation.likelihood(seq[i],qual[i],alpha,geno1,geno2)
       }
-      tmplike2=tmplike2+e^loglik*genotype.prior(geno2)
+      tmplike2=tmplike2+exp(loglik)*genotype.prior(geno2)
     }
     likelihood=likelihood+tmplike2*genotype.prior(geno1)
   }
   return(likelihood)
 }
-overall.likelihood=function()
+
+#data has the pileup format: Chrom\tPos\tSNPID\tDepth\tBases\tQualities
+overall.likelihood=function(data,ploidy1,ploidy2,alpha)
 {
-  
+  loglikelihood=0;
+  for(i in length(data[,1]))
+  {
+    seq=as.vector(data[i,5])
+    seq=strsplit(toupper(seq),"")[[1]]
+    qual=as.vector(data[i,6])
+    qual=strsplit(qual,"")[[1]]
+    #print(seq[1])
+    loglikelihood=loglikelihood+site.likelihood(seq,qual,ploidy1,ploidy2,alpha)
+  }
+  return(exp(loglikelihood))
 }
 
+
+pileup=read.table(gzfile("C:/Users/PC/Desktop/HG00553.large_insert.Pileup.gz"),header=F,comment.char="",quote="\"")
 
 
 ploidy.estimate=function()
